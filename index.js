c3497e2b6030d4028f7401fb5b773847983e5675
Andrew Eardley <eardley.andrew1@gmail.com>
2024-06-21 12:34:26 -0600

    methods return more useful data

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.post('/sandwiches', async function(req, res) {
 	try {
 		let sandwich = new Sandwich({bread: req.body.bread, layers: req.body.layers, protein: req.body.protein});
 		await sandwich.save(); 
-		res.status(200).send("Created");
+		res.status(200).json(sandwich);
 	}
 
 	catch (error) {
@@ -39,7 +39,7 @@ app.put('/sandwiches/:id', async function(req, res) {
 		let body = req.body;
 		let id = req.params.id;
 		await Sandwich.findByIdAndUpdate(id, body);
-		res.status(200).send("updated");
+		res.status(200).send("updated " + id);
 	}
 	
 	catch (error) {
@@ -51,7 +51,7 @@ app.put('/sandwiches/:id', async function(req, res) {
 app.delete('/sandwiches/:id', async function(req, res) {
 	let id = req.params.id;
 	await Sandwich.findByIdAndDelete(id);
-	res.status(200).send("deleted");
+	res.status(200).send("deleted " + id);
 })
 
 app.listen(process.env.PORT || 8080);
